668af396b89000df08260ddfe8f717df4c855f2e
KelvinTrigoni <kelvin.trigoni@formahomolog.com.br>
2021-06-06 22:14:01 -0300

    commit final

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,10 +2,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './views/login/login.component';
 import { AuthGuard } from './auth.guard';
+import { PdfComponent } from './views/pdf/pdf.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: 'pdf', component: PdfComponent },
   { path: 'home', loadChildren: () => import('./views/home/home.module').then(m => m.HomeModule), canActivate: [AuthGuard]},
 ];
 

b171314f0eb0ab7fdc8c6765c80bfe8c1fc3293d
KelvinTrigoni <50180555+KelvinTrigoni@users.noreply.github.com>
2021-06-13 23:40:30 -0300

    Update and rename src/environments/environment.ts to src/ambientes/environment.ts

---
+++ src/ambientes/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  guentai: 'https://guentaaiapi.azurewebsites.net/api'
+};
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  guentai: 'https://bbf5e12f058c.ngrok.io/api'
-};
